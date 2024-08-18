18238e385ea4f14985c0ffa367e1a0ec99778474
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-08-18 14:03:48 +0530

    update addTodo function to post to backend

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,11 +1,13 @@
 import { useState, useRef } from 'react';
 import { v4 as uuid } from 'uuid';
+import axios from 'axios';
 
 function CreateTodo({ todolist, setTodolist }) {
 	const [newItem, setNewItem] = useState('');
 	const inputRef = useRef();
 
-	const addTodo = () => {
+	const addTodo = async () => {
+		// add a tempId and optimistically update the UI
 		const tempId = uuid();
 		const newTodo = {
 			_id: tempId,
@@ -13,9 +15,31 @@ function CreateTodo({ todolist, setTodolist }) {
 			description: newItem,
 		};
 		setTodolist([newTodo, ...todolist]);
+
+		try {
+			// Create todo in mongoDB
+			const response = await axios.post('http://localhost:8080/new', {
+				description: newItem,
+			});
+			console.log(response);
+			// Update the todo list with the actual ID from MongoDB
+			setTodolist((prevTodolist) =>
+				prevTodolist.map((todo) =>
+					todo._id === tempId ? { ...todo, _id: response.data.todo._id } : todo
+				)
+			);
+		} catch (error) {
+			alert('Error creating todo');
+			// Rollback optimistic update if the request fails
+			setTodolist((prevTodolist) =>
+				prevTodolist.filter((todo) => todo._id !== tempId)
+			);
+		}
+
 		setNewItem('');
 		inputRef.current.focus();
 	};
+	
 	return (
 		<>
 			<input
